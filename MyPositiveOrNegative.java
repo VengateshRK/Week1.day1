60f1166f5dadd91f33608e123c5a2ca17036d0a0
VengateshRK <39152848+VengateshRK@users.noreply.github.com>
2021-09-04 21:56:54 +0530

    Add files via upload

---
+++ MyPositiveOrNegative.java
@@ -0,0 +1,19 @@
+package Week1.day1;
+
+public class MyPositiveOrNegative {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i= 35;
+		
+		if (i < 0) {
+			System.out.println("negative value  = " +i);
+			
+		}
+		else {
+			System.out.println("Positive value" +i);
+		}
+
+	}
+
+}
